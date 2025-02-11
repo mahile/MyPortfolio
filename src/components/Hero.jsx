73c98698d738d4889d645bcf8ec3f09bf046645c
Mah_Sipa <mahihone61@gmail.com>
2025-02-11 09:32:42 -0800

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,40 @@
+import { HERO } from '../constants';
+import carlImg from '../assets/m5.png'
+import { motion } from 'framer-motion';
+
+const Hero = () => {
+  return (
+    <section className='flex min-h-screen flex-wrap items-center'>
+    <motion.div
+    initial={{opacity:0,x:-50}}
+    animate={{opacity:1,x:0}}
+    transition={{duration:1}}
+    className='w-full md:w-1/2'>
+    <h2 className='my-8 p-2 text-4xl font-bold md:text-5xl lg:text-[5rem]'>
+  {HERO.name}
+</h2>
+<p className='p-2 text-3xl tracking-tighter lg:text-2xl'>
+  {HERO.greet}
+</p>
+<p className='mb-8 p-2 text-3xl tracking-tighter lg:text-2xl'>
+{HERO.description}
+</p>
+    </motion.div>
+    <motion.div 
+    initial={{opacity:0,scale:0.8}}
+    animate={{opacity:1,scale:1}}
+    transition={{duration:1}}
+    className='w-full md:w-1/2 lg:p-8'>
+    <div className='flex justify-center'>
+      <motion.img 
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      transition={{duration:1}}
+      src={carlImg} width={530} height={530} alt='mah' className='rounded-3xl'/>
+    </div>
+    </motion.div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/constants/index.jsx
@@ -0,0 +1,168 @@
+import {
+  FaXTwitter,
+  FaGithub,
+  FaLinkedin,
+  FaFacebook,
+  FaDiscord,
+  FaInstagram,
+} from "react-icons/fa6";
+
+import projectImage1 from "../assets/project1.jpeg";
+import projectImage2 from "../assets/project1.jpeg";
+import projectImage3 from "../assets/project1.jpeg";
+import projectImage4 from "../assets/project1.jpeg";
+import projectImage5 from "../assets/project1.jpeg";
+import projectImage6 from "../assets/project1.jpeg";
+
+import { RiReactjsLine } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiMongodb } from "react-icons/si";
+import { DiRedis } from "react-icons/di";
+import { FaNodeJs } from "react-icons/fa";
+import { BiLogoPostgresql } from "react-icons/bi";
+
+export const NAVIGATION_LINKS = [
+  { label: "Projects", href: "#projects" },
+  { label: "Bio", href: "#bio" },
+  { label: "Skills", href: "#skills" },
+  // { label: "Work Experience", href: "#work" },
+  // { label: "Education", href: "#education" },
+  { label: "Contact", href: "#contact" },
+];
+
+export const HERO = {
+  name: "Mahilet",
+  greet: "Hello there! 👋🏻",
+  description:
+    "I am a passionate frontend developer with a knack for creating beautiful and functional user interfaces. I like transforming ideas into engaging web experiences for over a decade.",
+};
+
+export const PROJECTS = [
+  {
+    id: 1,
+    name: "Personal Portfolio",
+    description:
+      "A personal portfolio website built with React and Tailwind CSS to showcase my skills, projects, and contact information.",
+    image: projectImage1,
+    githubLink: "https://github.com/user/personal-portfolio",
+  },
+  {
+    id: 2,
+    name: "E-Commerce Platform",
+    description:
+      "An e-commerce web application developed using ReactJs , featuring a comprehensive product catalog, shopping cart, and checkout system.",
+    image: projectImage2,
+    githubLink: "https://github.com/user/ecommerce-platform",
+  },
+  {
+    id: 3,
+    name: "Finance Managemnet System",
+    description:
+      "This is a full-stack project built with React (with Chakra UI) for the frontend and Node.js (with Express) for the backend.",
+    image: projectImage3,
+    githubLink: "https://github.com/mahile/FinanceManagement",
+  },
+  {
+    id: 4,
+    name: "Pharmacy Desktop app",
+    description:
+      "This is a desktop application called Pharmacy using JavaFX, which allows users to add, update, and delete medicines efficiently.",
+    image: projectImage4,
+    githubLink: "https://github.com/mahile/Pharmacy",
+  },
+
+];
+
+export const BIO = [
+  "Mahilet Honelign studing Software Enigneering at the University of Wolkite., where I immersed myself in the world of frontend development.",
+  "As a multi-disciplinary frontend developer,I brings a wealth of skills and expertise to my work. From my deep understanding of HTML, CSS, and JavaScript to my proficiency in modern frameworks like React and Vue.js,I possesses a versatile skill set that allows me to craft stunning and unforgettable web experiences for my clients and audiences alike.",
+];
+
+export const SKILLS = [
+  {
+    icon: <RiReactjsLine className="text-4xl text-cyan-400 lg:text-5xl" />,
+    name: "React",
+    experience: "6+ months",
+  },
+  {
+    icon: <TbBrandNextjs className="text-4xl text-white lg:text-5xl" />,
+    name: "Next.js",
+    experience: "6+ months",
+  },
+  {
+    icon: <SiMongodb className="text-4xl text-green-600 lg:text-5xl" />,
+    name: "MongoDB",
+    experience: "6+ months",
+  },
+ 
+  {
+    icon: <FaNodeJs className="text-4xl text-green-600 lg:text-5xl" />,
+    name: "Node.js",
+    experience: "6+ months",
+  },
+];
+
+export const EXPERIENCES = [
+  {
+    title: "Lead Frontend Developer",
+    company: "Innovative Tech Solutions",
+    duration: "July 2020 - Present",
+    description:
+      "As the Lead Frontend Developer, I spearheaded the development of advanced web applications using cutting-edge technologies like React, Redux, and TypeScript. I worked closely with cross-functional teams, including designers, product managers, and backend developers, to deliver seamless and high-performance user experiences.",
+  },
+  {
+    title: "Frontend Engineer",
+    company: "Digital Creations",
+    duration: "February 2016 - June 2020",
+    description:
+      "At Digital Creations, I focused on building highly interactive and responsive web interfaces using HTML, CSS, JavaScript, and modern libraries like React. I collaborated closely with UX/UI designers to implement design changes that enhanced user engagement and satisfaction. My role involved optimizing website performance, ensuring cross-browser compatibility, and implementing SEO best practices. ",
+  },
+  {
+    title: "Junior Web Developer",
+    company: "Bright Future Technologies",
+    duration: "August 2014 - January 2016",
+    description:
+      "In my role as a Junior Web Developer, I assisted in the development and maintenance of various web applications. I gained hands-on experience in utilizing HTML, CSS, and JavaScript to create user-friendly interfaces. I actively participated in team meetings, contributed to project planning, and collaborated with senior developers to implement new features.",
+  },
+];
+
+export const EDUCATION = [
+  {
+    degree: "Master of Science in Computer Science",
+    institution: "Stanford University",
+    duration: "September 2012 - June 2014",
+    description:
+      "Specialized in Human-Computer Interaction and Software Engineering. Completed a thesis on enhancing user experience in web applications through advanced interactive techniques. Participated in various projects involving frontend development, algorithms, and data structures. Graduated with honors.",
+  },
+  {
+    degree: "Bachelor of Science in Information Technology",
+    institution: "University of California, Berkeley",
+    duration: "September 2008 - June 2012",
+    description:
+      "Focused on web development, programming languages, and database management. Actively involved in coding clubs and hackathons, where I developed several web applications using HTML, CSS, JavaScript, and PHP. Completed a senior project on developing an e-commerce platform. Graduated with a high GPA.",
+  },
+];
+
+export const SOCIAL_MEDIA_LINKS = [
+  {
+    href: "https://x.com/",
+    icon: <FaFacebook fontSize={25} className="hover:opacity-80" />,
+  },
+
+  {
+    href: "https://x.com/",
+    icon: <FaInstagram fontSize={25} className="hover:opacity-80" />,
+  },
+  {
+    href: "https://x.com/",
+    icon: <FaXTwitter fontSize={25} className="hover:opacity-80" />,
+  },
+  {
+    href: "https://github.com/",
+    icon: <FaGithub fontSize={25} className="hover:opacity-80" />,
+  },
+  {
+    href: "https://www.linkedin.com/",
+    icon: <FaLinkedin fontSize={25} className="hover:opacity-80" />,
+  },
+];
